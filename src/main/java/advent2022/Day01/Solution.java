8f25bea2cc418e7c5ae1dd97bcc8712d683a2843
Kacper Krasowski <kacperkrasowski@wp.pl>
2025-09-01 22:56:33 +0200

    simple version

--- src/main/java/advent2022/Day01/Solution.java
+++ src/main/java/advent2022/Day01/Solution.java
@@ -1,37 +1,42 @@
 package advent_2022.AoC1;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
 
-    private int sumTopCalories(List<String> calorieSums, int topCount) {
-        if (topCount > calorieSums.size()) {
-            return 0;
+
+    private int sumTopGroups(List<String> rawInput, int topCount) {
+        List<Integer> summedCalories = new ArrayList<>();
+
+        int sum = 0;
+        for (String number : rawInput) {
+
+            if (!number.isEmpty()) {
+                sum += Integer.parseInt(number);
+            } else {
+                summedCalories.add(sum);
+                sum = 0;
+            }
         }
-        int sumTop = Arrays.stream(String.join(" ", calorieSums).split("  "))
-                .mapToInt(group -> Arrays.stream(group.trim().split(" "))
-                        .filter(s -> !s.isEmpty())
-                        .mapToInt(Integer::parseInt)
-                        .sum()
-                )
-                .boxed()
-                .sorted(Comparator.reverseOrder())
-                .limit(topCount)
-                .mapToInt(Integer::intValue)
-                .sum();
 
-        return sumTop;
+        if (sum > 0) {
+            summedCalories.add(sum);
+        }
 
+
+        summedCalories.sort(Collections.reverseOrder());
+        int totalSum = 0;
+        for (int i = 0; i < topCount && i < summedCalories.size(); i++) {
+            totalSum += summedCalories.get(i);
+        }
+
+        return totalSum;
     }
 
     public void run() {
         FileHandler fileHandler = new FileHandler();
         List<String> rawCalories = fileHandler.getConvertedListFromFile();
-
-
-        System.out.println(sumTopCalories(rawCalories, 1));
-        System.out.println(sumTopCalories(rawCalories, 3));
+        System.out.println(sumTopGroups(rawCalories, 1));
+        System.out.println(sumTopGroups(rawCalories, 3));
     }
 }
